77102239e04ae03d1db53528349de779a4693251
ishika <ishikatakhtani04@gmail.com>
2025-01-07 18:30:02 +0530

    update

---
+++ bom.js
@@ -0,0 +1,40 @@
+function bom(){
+
+// window.alert("This is an alert message")
+
+// let abc=window.confirm("this is confrim box")
+// console.log(abc);
+
+// let show=prompt("Are you happy with our website")
+// console.log(show);
+
+
+// let display=prompt("Using prompt and alert")
+// window.alert(display+" thanks for ur response")
+
+
+//CHECKING IF NO. IS ODD OR EVEN 
+
+//SOL:-
+// let a=5;
+// if(a%2==0){
+//     alert("even no.")
+// }
+// else{
+//     alert("odd no.")
+// }
+
+//CHECKING IF NO. IS ODD OR EVEN  taking input from user
+
+//SOL:-
+
+let b=prompt("enter a no.")
+if(b%2==0){
+    alert("even")
+}
+else{
+    alert("odd")
+}
+}
+bom()
+
