48ed7d3fc0b479e0aa4a97a151d00e845c0e5735
ishika <ishikatakhtani04@gmail.com>
2025-01-08 18:24:43 +0530

    update

--- bom.js
+++ bom.js
@@ -39,12 +39,62 @@ function bom(){
 // bom()
 
 // take 2 prompt , take 2 no. and show answer in alert
-//Sol
-let a= parseInt(prompt("enter n"))
-let b=parseInt(prompt("enter b"))
-alert(a+b);
+// Sol
+
+// let a= parseInt(prompt("enter n"))
+// let b=parseInt(prompt("enter b"))
+// alert(a+b);
 
 }
 bom()
 
+
+//                                                      DAY 2 BOM
+
+
+
+function bom2(){
+    //QUESTION 1
+    //ask user age and according to age display yes u can vote or not
+    // let age=prompt("enter age")
+    // if(age>=18){
+    //     alert("You can vote")
+    // }
+    // else{
+    //     alert("you can not vote")
+    // }
+                               //METHOD 2 QUSTION 1
+
+    //TERNORY 
+    //SYNTAX:- condition?"on true":"on false"
+    // age>=18?alert("vote"):alert("not");
+
+//QUESTION 2
+    //if user clicks on ok show thank you if cancel  show sorry
+// let check=confirm("did u like are website")
+// if(check==true){
+//     alert("thank you")
+// }
+// else{
+//     alert("sorry")
+// }
+                                 //METHOD 2 QUSTION 2
+//TERNORY
+// check==true?alert("thank you"):alert("sorry")
+// }
+
+   //FOR LOOP
+                                 // QUSTION 1
+
+                                 for(let n=0;n<=10;n++){
+                                    console.log(n);
+                                    
+                                }
+                            }
+
+bom2()
+
+
+
+                             
 
