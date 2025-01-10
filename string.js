f62437783c431bd1a1c38b856ff6e6ec4634597e
ishika <ishikatakhtani04@gmail.com>
2025-01-10 18:27:26 +0530

    update

--- string.js
+++ string.js
@@ -1,10 +1,24 @@
-function f1(){
-    alert("USING EVENT LISTERN FUNCTION")
-}
-// f1()
+// function f1(){
+//     alert("USING EVENT LISTNER FUNCTION")
+// }
+// // f1()
+// function f2(){
+//     alert("ishika")    printts on browser window
+// }
+// function f3(){
+//     alert("javascript")
+// }
+
+
+//                              -----------------------------DAY2----------------------------------
 function f2(){
-    alert("ishika")
+  //Now we will write on html page , done using document.write("write text")
+let text2="text"
+let text="javaScriptIsEasy"
+// document.write(text.length)  method to check length string 
+// document.write(text.slice(4,13))   method for slice of string 
+// document.write(text.concat(text2))   method for joining two text ie concat method
+document.write(text.toUpperCase())
+document.write(text.toLowerCase())
 }
-function f3(){
-    alert("javascript")
-}
+f2()
