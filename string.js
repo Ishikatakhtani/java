bb584e3bf61c72c1ba9926820ac5e8b1c95c9cc0
ishika <ishikatakhtani04@gmail.com>
2025-01-11 17:08:39 +0530

    update

--- string.js
+++ string.js
@@ -13,12 +13,20 @@
 //                              -----------------------------DAY2----------------------------------
 function f2(){
   //Now we will write on html page , done using document.write("write text")
-let text2="text"
-let text="javaScriptIsEasy"
+// let text2="text"
+// let text="javaScriptIsEasy"
 // document.write(text.length)  method to check length string 
 // document.write(text.slice(4,13))   method for slice of string 
 // document.write(text.concat(text2))   method for joining two text ie concat method
-document.write(text.toUpperCase())
-document.write(text.toLowerCase())
+// document.write(text.toUpperCase())    method for converting text to upper case
+// document.write(text.toLowerCase())       method for converting text to lower case
 }
 f2()
+    function f3(){
+    let text1="Homework"
+    // console.log(text1.charAt(1));
+    // console.log(text1.charCodeAt(0));
+    // console.log(text1.indexOf("o"));
+    // console.log(text1.split("e"));
+    }
+    f3()
