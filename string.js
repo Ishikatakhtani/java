c7b24a9a8de8837081c3fa6c63c63944707f2874
ishika <ishikatakhtani04@gmail.com>
2025-01-11 18:30:17 +0530

    update

--- string.js
+++ string.js
@@ -15,18 +15,32 @@ function f2(){
   //Now we will write on html page , done using document.write("write text")
 // let text2="text"
 // let text="javaScriptIsEasy"
-// document.write(text.length)  method to check length string 
-// document.write(text.slice(4,13))   method for slice of string 
-// document.write(text.concat(text2))   method for joining two text ie concat method
-// document.write(text.toUpperCase())    method for converting text to upper case
-// document.write(text.toLowerCase())       method for converting text to lower case
+// document.write(text.length)         method to check length string 
+// document.write(text.slice(4,13))    method for slice of string 
+// document.write(text.concat(text2))  method for joining two text ie concat method
+// document.write(text.toUpperCase())  method for converting text to upper case
+// document.write(text.toLowerCase())  method for converting text to lower case
 }
 f2()
+
     function f3(){
-    let text1="Homework"
+    let text1="Home-work-done"
     // console.log(text1.charAt(1));
     // console.log(text1.charCodeAt(0));
     // console.log(text1.indexOf("o"));
-    // console.log(text1.split("e"));
+    console.log(text1.split("-"));
     }
     f3()
+// ----------------------------------------------------DAY3--------------------------------------------------------------------------
+ function f4(){
+  let text="I am leraning Javascript and Javascript is fun"
+  // document.write(text.replace("Javascript","JS"))    //this will convert only 1 javascript which will br found first.
+// document.write(text.replace(/Javascript/g,"JS"))     //this will convert all in same case javascript   
+// document.write(text.replace(/Javascript/gi,"JS")) 
+//g is global  
+//i is for case insensitive
+// let arr=[2,3,4,"ishika"]
+// document.write(arr)
+
+ }
+//  f4()
