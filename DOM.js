98145b7c3ae9d10640462fcd55061f9b9b03f458
ishika <ishikatakhtani04@gmail.com>
2025-01-14 18:38:09 +0530

    updation

---
+++ DOM.js
@@ -0,0 +1,5 @@
+function f1(){
+//    document.getElementById("text") //Method 1
+//   let a= document.querySelector("#text")  //Method 2
+//   a.innerHTML="1 class of DOM"
+}
--- array.js
+++ array.js
@@ -1,10 +1,19 @@
 function intro(){
-    let arr1=["apple","mango","grapes"]
+    let arr1=["apple","mango","grapes","apple","mango","grapes"]
     console.log(arr1.length); //length
 //    arr1.pop() //removes last elemnent 
 // arr1.push("papaya") //enters element from last
 // arr1.shift()  //removes element from start
 // arr1.unshift("fruits")  //adds element from start
+// let arr2=[1,2]
+// let arr3=arr1.concat(arr2);
+// arr1.reverse()
+// arr2=arr1.slice(1,3)   //imp
+// arr1.splice(2,0,"add on 2 and 0for no deletion") // imp
+// arr1.splice(2,0,1) //imp
+// arr1.sort()
+// arr1.reverse()
+//slice concat reverse sort dont change the original array 
 console.log(arr1);
 
 }
