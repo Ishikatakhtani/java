c78429f814b326f8072d95073cd61b0c9d913f66
ishika <ishikatakhtani04@gmail.com>
2025-01-17 17:51:07 +0530

    updation

--- DOM.js
+++ DOM.js
@@ -2,6 +2,9 @@ function f1(){
 //    document.getElementById("text") //Method 1
   let a= document.querySelector("#text")  //Method 2
   a.innerHTML="1 class of DOM"
-a.style.color="green";
+// a.style.color="green";
 a.style.backgroundColor="red";
+
 }
+
+// TEST DONE 
