1af0b25e1898aa9d2f617755d22a2a672b42d031
ishika <ishikatakhtani04@gmail.com>
2025-01-18 18:12:27 +0530

    updation

--- DOM.js
+++ DOM.js
@@ -1,10 +1,21 @@
 function f1(){
 //    document.getElementById("text") //Method 1
-  let a= document.querySelector("#text")  //Method 2
-  a.innerHTML="1 class of DOM"
+
+  // let a= document.querySelector("#text")  //Method 2
+  // a.innerHTML="1 class of DOM"
 // a.style.color="green";
-a.style.backgroundColor="red";
+// a.style.backgroundColor="red";
 
 }
-
-// TEST DONE 
+// function f2(){
+// console.log(a);
+// const a=0
+// }
+f2()
+//                                                                          DAY 2
+function f3(){
+  let a=document.querySelector("#t")
+  t.innerHTML="<h1>hello</h1>";
+  t.style.color="red";
+  
+}
