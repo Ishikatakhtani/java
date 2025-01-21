2759e07c6549afd8848e60855f2e97470fc97455
ishika <ishikatakhtani04@gmail.com>
2025-01-21 17:31:56 +0530

    updation

--- object.js
+++ object.js
@@ -13,3 +13,23 @@ let objj={
 // objj.age=21                         //updating a key 
 // console.log(objj.age)
 
+// delete objj.city                      //delete a key 
+// console.log(objj);
+
+let objj1={
+    //IMP QUESTION
+    name:"ishika",
+    age:25,
+    city:"Bhopal",
+    city:"ahemdabad",
+    fruits:["apple","mango","orange",],
+   fun: function f1(){
+console.log(`hello your name and age is  ${this.name} ${this.age}`);   //IMP TEMPLEMENT LSTTERAL
+    }
+}
+// console.log(objj1);
+// console.log(objj1.fruits[2]);
+//  objj1.fun()                         
+
+
+
