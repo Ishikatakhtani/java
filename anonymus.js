1d5c986e0c4f227291a8db2e1441f85214a40bb0
ishika <ishikatakhtani04@gmail.com>
2025-01-23 17:28:13 +0530

    updation

---
+++ anonymus.js
@@ -0,0 +1,29 @@
+// normal way of function declaration 
+// function f1(){
+
+// }
+// f1()
+
+//Anonymus way- in this function has no name so we assing it a variable 
+// let anonymus=function()
+// {
+// console.log("hello")
+// }
+// anonymus()
+
+
+//IIFE -> Immediately invoked function expression
+// (
+    // function(){
+    //     console.log("IIFE")
+    // }
+// )()
+
+
+// FUNCTION DECLARATION WE WILL USE 
+// ARROW Function(=>)      Special type of anoymus function
+
+    let arrow=()=>{
+        console.log("arrow function imp")
+    }
+    arrow()
