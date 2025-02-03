caccc14f6f25f24ec27c83e549f739bb08201403
ishika <ishikatakhtani04@gmail.com>
2025-02-03 17:22:17 +0530

    updation

--- form.js
+++ form.js
@@ -11,11 +11,16 @@ let erremail=document.querySelector("#erroremail");
 let errpass=document.querySelector("#errorpassword");
 let errcpass=document.querySelector("#errorcpass");
 if(ipname==""){
+    let nameinput=document.querySelector("#name")
+     nameinput.style.border= "1px solid red"
+    nameinput.placeholder="enter name "
     errname.innerHTML="please enter you name"
     errname.style.color="red"
     return false
 }
 else if(ipnumber=="" ){
+
+     errname.innerHTML=""
     errnumber.innerHTML="please enter your number"
     errnumber.style.color="red"
     return false
@@ -33,6 +38,7 @@ else if(ipnumber.length!==10){
 
 
 else if(ipemail==""){
+     errnumber.innerHTML=""
     erremail.innerHTML="please enter your email"
     erremail.style.color="red"
     return false
@@ -45,6 +51,7 @@ else if(!(ipemail.includes('@')&&ipemail.includes(".com"))){
 
 
 else if(ippassword=="" ){
+    erremail.innerHTML=""
         errpass.innerHTML="please enter your password"
         errpass.style.color="red"
         return false
@@ -52,11 +59,15 @@ else if(ippassword=="" ){
 }
 
 else if(ipcpass==""){ 
+     errpass.innerHTML=""
     errcpass.innerHTML="please enter your confrim password"
     errcpass.style.color="red"
     return false
 }
 else if(ippassword!=ipcpass){
+document.querySelector("#cpass").value="";
+document.querySelector("#pass").value="";
+document.querySelector("#cpass").focus();  //to bring cusror on place where we want user to enter pass
     errpass.innerHTML="please enter correct password"
     errpass.style.color="red"
     return false
@@ -64,8 +75,9 @@ else if(ippassword!=ipcpass){
 
 else if(!( ippassword.match([/ 1234567890/]&&ippassword.match([/ !@#$%^&*()/])&&ippassword.match([/ a-z/])&&ippassword.match([/ A-Z/])))){
     errpass.innerHTML="please enter correct password"
-    errpass.style.color="red"
+    errpass.style.color="green"
     return false  
 }
+
 
 }
