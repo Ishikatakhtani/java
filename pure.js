f7f08b79c1065a6e887c28792f3ff68516a64f6b
ishika <ishikatakhtani04@gmail.com>
2025-02-04 17:21:04 +0530

    updation

---
+++ pure.js
@@ -0,0 +1,9 @@
+let a=document.querySelector("#btn")
+a.addEventListener("click",()=>{
+    alert("pure java")
+})
+
+let b=document.querySelector("#d")
+b.addEventListener("dblclick",()=>{
+    b.style.backgroundColor="green"
+})
