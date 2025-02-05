63b1c06fa32a6c2045207056049be05e06c42459
ishika <ishikatakhtani04@gmail.com>
2025-02-05 16:56:09 +0530

    updation

--- pure.js
+++ pure.js
@@ -1,9 +1,34 @@
-let a=document.querySelector("#btn")
-a.addEventListener("click",()=>{
-    alert("pure java")
-})
+// let a=document.querySelector("#btn")
+// a.addEventListener("click",()=>{
+//     alert("pure java")
+// })
 
-let b=document.querySelector("#d")
-b.addEventListener("dblclick",()=>{
-    b.style.backgroundColor="green"
+// let b=document.querySelector("#d")
+// b.addEventListener("dblclick",()=>{
+//     b.style.backgroundColor="green"
+// })
+
+// let a=()=>{
+
+//     alert("pure java")
+// }
+
+// let inpbox=document.querySelector("#inpbox")
+// inpbox.addEventListener("keydown",(e)=>{
+//     console.log(e.key);
+    
+// })
+// inpbox.addEventListener("input".at()=>{
+    
+// })
+
+//keydown-tells which key we pressed
+//input-
+
+let a=document.querySelector("#i")
+
+a.addEventListener("input",()=>{
+
+   let inp=document.querySelector("#h")
+   inp.innerHTML=a.value
 })
