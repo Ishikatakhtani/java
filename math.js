37699aa960983f8466a995ced1bbdda08a6fdba4
ishika <ishikatakhtani04@gmail.com>
2025-02-07 17:33:12 +0530

    updation

---
+++ math.js
@@ -0,0 +1,14 @@
+// document.write(Math.PI);
+
+// document.write(Math.abs(-9));
+
+// document.write(Math.floor(Math.random()*1000+1000));
+
+//to get answer between 1-10 multiple by 10 
+//bydefault it returns value between 0-1
+//if we dont want decimalwe use Math.floor
+
+let ran=()=>{
+
+        document.write(Math.floor(Math.random()*6+1))
+     }
