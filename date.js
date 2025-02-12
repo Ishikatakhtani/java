8d07520b02433ad36a3c1252e67fe76b92367fda
ishika <ishikatakhtani04@gmail.com>
2025-02-12 17:09:43 +0530

    updation

---
+++ date.js
@@ -0,0 +1,18 @@
+
+// let a=new Date();
+// console.log(a.toLocaleString())
+// console.log(a)
+// let b=a.getFullYear()
+// console.log(b.toLocaleString())
+// let c=a.getDate()
+// let d=a.getHours()
+// console.log(c.toLocaleString())
+// console.log(d.toLocaleString())
+// let e=a.getMonth()
+// console.log(e.toLocaleString())
+// let f=a.getDay()
+// console.log(f.toLocaleString());
+
+// console.log(new Date());
+
+
