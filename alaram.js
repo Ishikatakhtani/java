3394bc6349f1872866954601e5934bd85764fcf3
ishika <ishikatakhtani04@gmail.com>
2025-02-14 17:21:21 +0530

    updation

--- alaram.js
+++ alaram.js
@@ -1,18 +1,42 @@
+// let v=document.querySelector("#in").value
+// let show=document.querySelector("#h")
 let set=()=>{
     
     let v=document.querySelector("#in").value
-    
+    let show=document.querySelector("#h")
+    let aud=document.querySelector("#ad")
     chg=setInterval( ()=>{
         let d=new Date()
-       let h=`${d.getHours()}:${d.getMinutes()}`
-       
-      
-       let show=document.querySelector("#h")
-      
-    //    show.innerHTML=`${h} :${m}:${ss}:${s}`
-       
-        })
+       let h=`${d.getHours().toString().padStart(2,"0")}:${d.getMinutes().toString().padStart(2,"0")}`
     if(v==h){
-    sho    
-    }
+      show.innerHTML="ring" 
+      aud.play()
+      }
+// else{
+//   show.innerHTML="alaram is set"
+// }
+
+setTimeout( ()=>{
+  clearInterval(aud.pause())
+       
+   },6000)
+
+  
+     
+        },1000)
+        
+    
 }
+
+// let g=()=>{
+//  }
+    
+ 
+
+// let s=()=>{
+   
+    
+//     let s2=document.querySelector("#hh") 
+//     s2.innerHTML="stopped"  
+    
+//     }
